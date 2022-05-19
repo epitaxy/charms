0399f823c803e25b9065c1bc4a46de9d58703424
epitaxy <31850335+epitaxy@users.noreply.github.com>
2022-05-19 20:27:57 +0900

    Update ubuntu_mates.sh

--- ubuntu_mates.sh
+++ ubuntu_mates.sh
@@ -2,3 +2,9 @@
 
 # Faster Ubuntu APT repository server for Korean
 sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+
+# essential packages
+sudo apt install -y nnn zsh ncdu htop rsync
+
+# Installation of om my zsh 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
