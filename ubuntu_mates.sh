6856c8d166e6bf6249be271c0f872a101ab0a840
epitaxy <31850335+epitaxy@users.noreply.github.com>
2022-05-24 14:49:33 +0900

    Update ubuntu_mates.sh

--- ubuntu_mates.sh
+++ ubuntu_mates.sh
@@ -5,7 +5,7 @@ sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 
 # essential packages
-sudo apt install -y nnn zsh ncdu htop rsync git
+sudo apt install -y nnn zsh ncdu htop rsync git vim
 
 # Installation of om my zsh 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
